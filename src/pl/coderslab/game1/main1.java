1e7f6cd3315b5b88cdc7396e42c42e3e1eccf417
Diana Dobrzanska <dianadobrzanska@gmail.com>
2019-07-26 15:56:09 +0200

    git init

---
+++ src/pl/coderslab/game1/main1.java
@@ -0,0 +1,20 @@
+package pl.coderslab.game1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class main1 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int rand_int1 = rand.nextInt(101);
+
+    }
+    public static void scanner(){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Zgadnij liczę");
+
+
+    }
+}
